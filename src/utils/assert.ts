610d0ded1f192fdc236947d7f13ea6f5cf5e6bbf
Soda <62868605+JericN@users.noreply.github.com>
2024-06-25 23:14:49 +0800

    feat: add assert methods

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -1,3 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function assert(condition: any, msg = 'assertion failed'): asserts condition {
     if (!condition) throw new Error(msg);
+}
+
+export function assertDefined<T>(value: T | undefined, msg = 'value is not defined'): asserts value is T {
+    if (typeof value === undefined) {
+        throw new Error(msg);
+    }
+}
+
+export function assertType<T>(value: unknown, expectedType: string): asserts value is T {
+    if (typeof value !== expectedType) {
+        throw new TypeError(`Expected type to be '${expectedType}', but received type '${typeof value}'`);
+    }
 }
