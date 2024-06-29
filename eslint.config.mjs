55e4734ade0e029c3e7cb6739cc24b2775453a52
Soda <62868605+JericN@users.noreply.github.com>
2024-06-29 19:48:44 +0800

    build: update lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,6 @@ export default [
             'dot-notation': 'error',
             eqeqeq: 'error',
             'func-style': ['error', 'declaration'],
-            'init-declarations': 'error',
             'logical-assignment-operators': 'error',
             'new-cap': 'error',
             'no-alert': 'warn',
